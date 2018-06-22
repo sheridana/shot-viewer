c6e4ea14eecc0c5d88ef4025a8b8f735bbced8fe
ikwak@osprey <ikwak@osprey>
2018-06-22 01:16:05 -0400

    moved static html files to static directory. working initial node.js server.

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-app.get('/', function (req, res) {
-    res.send('Hello World!')
-});
+// app.get('/', function (req, res) {
+//     res.send('Hello World!')
+// });
 
 app.listen(8000, function () {
     console.log('Example app listening on port 8000!')
 });
 
-// app.get('/', function (req, res) {
-//     res.sendFile(path.join(__dirname + '/index.html'));
-// });
+// app.use('/', express.static(__dirname + '/static'));
+app.use('/data', express.static(__dirname + '/static/data'));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/static/index.html'));
+});
