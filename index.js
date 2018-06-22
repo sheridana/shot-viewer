f95e9f4f30dba1eda90acc2adeb16acbf6e515ac
ikwak <ikwak@beastbox>
2018-06-21 21:10:45 -0400

    starting a node server to host files.

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+// app.get('/', function (req, res) {
+//   res.send('Hello World!')
+// })
+
+app.listen(8000, function () {
+    console.log('Example app listening on port 8000!')
+});
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
