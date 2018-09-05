f4a381014303340b92ae04779912069f09467f28
Arlo Sheridan <sheridana@janelia.hhmi.org>
2018-09-05 17:34:48 -0400

    Separate HTML, CSS, and JS

---
+++ static/shot_viewer.js
@@ -0,0 +1,158 @@
+var margin = {top: 20, right: 100, bottom: 30, left: 40},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+var x0 = function(d) { return d["location x"];},
+    y0 = function(d) { return d["location y"];},
+    x = d3.scaleLinear().domain(x0).range([0, width]),
+    y = d3.scaleLinear().domain(y0).range([height, 0]),
+    xMap = function(d){ return x(x0(d));};
+    yMap = function(d){ return y(y0(d));};
+
+var xAxis = d3.axisBottom()
+      .scale(x);
+
+    yAxis = d3.axisLeft()
+      .scale(y);
+
+var cValue = function(d) { return d["shoot player"];},
+    color = d3.scaleOrdinal(d3.schemeCategory10);
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+// add the tooltip area to the webpage
+var tooltip = d3.select("body").append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0);
+
+d3.csv("data/shot log CLE.csv", function(error, all_data) {
+  if(error) {
+      console.log(error);
+  }
+
+  // change string (from CSV) into number format
+  var data = [];
+  all_data.forEach(function(d) {
+
+      d["location x"] = +d["location x"];
+      d["location y"] = +d["location y"];
+      data.push(d);
+    // d["location x"] = +d["location x"];
+    // d["location y"] = +d["location y"];
+//    console.log(d);
+  });
+
+x.domain([d3.min(data, x0)-1, d3.max(data, x0)+1]);
+y.domain([d3.min(data, y0)-1, d3.max(data, y0)+1]);
+
+var brush = d3.brush().on("end", brushended),
+    idleTimeout,
+    idleDelay = 350;
+
+svg.selectAll(".dot")
+      .data(data)
+    .enter().append("circle")
+      .attr("class", "dot")
+      .attr("r", 3.5)
+      .attr("cx", xMap)
+      .attr("cy", yMap)
+      .style("fill", function(d) { return color(cValue(d));})
+      .on("mouseover", function(d) {
+          tooltip.transition()
+               .duration(200)
+               .style("opacity", 0.9);
+          tooltip.html(d["shoot player"] + "<br/> (" + x0(d) + ", " + y0(d) + ")")
+               .style("left", (d3.event.pageX + 5) + "px")
+               .style("top", (d3.event.pageY - 28) + "px");
+      })
+      .on("mouseout", function(d) {
+          tooltip.transition()
+               .duration(500)
+               .style("opacity", 0);
+      });
+
+// x-axis
+  svg.append("g")
+    .attr("class", "axis axis--x")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis)
+  .append("text")
+    .attr("class", "label")
+    .attr("x", width + 50)
+    .attr("y", 10)
+    .style("text-anchor", "end")
+    .text("Location X");
+
+svg.append("g")
+    .attr("class", "axis axis--y")
+    .attr("transform", "translate(0,0)")
+    .call(yAxis)
+  .append("text")
+    .attr("class", "label")
+    .attr("transform", "rotate(0)")
+    .attr("y", -20)
+    .attr("x", 10)
+    .attr("dy", ".71em")
+    .style("text-anchor", "end")
+    .text("Location Y");
+
+svg.selectAll(".domain")
+    .style("display", "none");
+
+svg.append("g")
+    .attr("class", "brush")
+    .call(brush);
+
+// draw legend
+var legend = svg.selectAll(".legend")
+    .data(color.domain())
+  .enter().append("g")
+    .attr("class", "legend")
+    .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+
+// draw legend colored rectangles
+legend.append("rect")
+    .attr("x", width + 80)
+    .attr("width", 18)
+    .attr("height", 18)
+    .style("fill", color);
+
+// draw legend text
+legend.append("text")
+    .attr("x", width + 75)
+    .attr("y", 9)
+    .attr("dy", ".35em")
+    .style("text-anchor", "end")
+    .text(function(d) { return d;});
+
+function brushended() {
+  var s = d3.event.selection;
+  if (!s) {
+    if (!idleTimeout) return idleTimeout = setTimeout(idled, idleDelay);
+    x.domain(x0);
+    y.domain(y0);
+  } else {
+    x.domain([s[0][0], s[1][0]].map(x.invert, x));
+    y.domain([s[1][1], s[0][1]].map(y.invert, y));
+    svg.select(".brush").call(brush.move, null);
+  }
+  zoom();
+}
+
+function idled() {
+  idleTimeout = null;
+}
+
+function zoom() {
+  var t = svg.transition().duration(750);
+  svg.select(".axis--x").transition(t).call(xAxis);
+  svg.select(".axis--y").transition(t).call(yAxis);
+  svg.selectAll("circle").transition(t)
+      .attr("cx", xMap)
+      .attr("cy", yMap);
+}
+})
